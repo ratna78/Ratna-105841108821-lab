96e3b8e458b076f07a5712617305baa5271e313c
ratna <105841108821@student.unismuh.ac.id>
2024-06-19 14:44:39 +0800

    Tugas Warna

--- App.js
+++ App.js
@@ -1,62 +1,21 @@
-import React from "react";
-import { View, Text, StyleSheet, Image, Button, Alert } from 'react-native';
-
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
 const App = () => {
   return (
-    <View style={styles.container}>
-      {/*Header Text*/}
-      <Text style={styles.headerText}>Facebook</Text>
-
-      {/* Center Image */}
-      <Image
-        source={require('./fb.jpeg')}
-        style={styles.image}
-      />
-
-      {/* Footer Buttons */}
-      <View style={styles.footer}>
-        <View style={styles.buttonContainer}>
-          <Button
-            title="Sign In"
-            onPress={() => Alert.alert('Sign In button clicked!')}
-          />
+    <View style={{flex: 1,
+    }}>
+      <View style={{ flex: 1, flexDirection:'row', backgroundColor: 'black'}}>
+        <View style={{ flex: 1, backgroundColor: 'black'}}>
         </View>
-        <View style={styles.buttonContainer}>
-          <Button
-            title="Login"
-            onPress={ () => Alert.alert('Login button clicked!')}
-          />
+        <View style={{ flex : 1,  backgroundColor: 'green'}}>
         </View>
       </View>
+        <View style={{ flex: 1, backgroundColor: 'brown'}}>
+      </View>
+      <View style={{ flex: 1, backgroundColor: 'blue'}}>
+      </View>
     </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    padding: 20,
-  },
-  headerText: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
-  image: {
-    width: 300,
-    height: 300,
-    resizeMode: 'contain',
-    marginBottom: 20,
-  },
-  footer: {
-    flexDirection: 'row',
-  },
-  buttonContainer: {
-    marginHorizontal: 10,
-  },
-
-});
+  )
+}
 
 export default App;
