296f12cedf681d844bc21d632a3fa3dad3d73dd0
ratna <105841108821@student.unismuh.ac.id>
2024-06-19 13:31:23 +0800

    Tugas Text Gambar SIGN IN DAN LOGIN

--- App.js
+++ App.js
@@ -1,43 +1,62 @@
-
-import { View, Text, StyleSheet, TextInput } from 'react-native'
-import React from 'react'
+import React from "react";
+import { View, Text, StyleSheet, Image, Button, Alert } from 'react-native';
 
 const App = () => {
-
-  const TextInputCustom = ({name, color}) => {
   return (
-   
-       <TextInput
-        placeholder={`Masukkan ${name}`}
-        
-        style={{
-          height: 40,
-          width: 200,
-          borderColor: 'gray',
-          borderWidth: 1,
-          padding: 10,
-          marginBottom: 10,
-          borderRadius: 10,
-          paddingLeft: 10,
-          color: color,
+    <View style={styles.container}>
+      {/*Header Text*/}
+      <Text style={styles.headerText}>Facebook</Text>
 
-        }}
-     />
-      
-    
-     
-  )
-  }
-  return(
-    <View style={{
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-    }}>
-      <TextInputCustom name="Username" color="balck"/>
-      <TextInputCustom name="Password" color="red" />
+      {/* Center Image */}
+      <Image
+        source={require('./fb.jpeg')}
+        style={styles.image}
+      />
+
+      {/* Footer Buttons */}
+      <View style={styles.footer}>
+        <View style={styles.buttonContainer}>
+          <Button
+            title="Sign In"
+            onPress={() => Alert.alert('Sign In button clicked!')}
+          />
+        </View>
+        <View style={styles.buttonContainer}>
+          <Button
+            title="Login"
+            onPress={ () => Alert.alert('Login button clicked!')}
+          />
+        </View>
+      </View>
     </View>
-  )
-}
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    padding: 20,
+  },
+  headerText: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 20,
+  },
+  image: {
+    width: 300,
+    height: 300,
+    resizeMode: 'contain',
+    marginBottom: 20,
+  },
+  footer: {
+    flexDirection: 'row',
+  },
+  buttonContainer: {
+    marginHorizontal: 10,
+  },
+
+})
 
 export default App;
