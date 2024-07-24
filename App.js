83b85db8e9d1248d838501a85602f113ce3c50e9
ratna <105841108821@student.unismuh.ac.id>
2024-07-24 14:15:35 +0800

    useHook data dan onChange

--- App.js
+++ App.js
@@ -1,31 +1,44 @@
-// In App.js in a new project
+import { StyleSheet, Text, View, TextInput, Button } from 'react-native'
+import React, { useState } from 'react'
+import { Button } from 'react-native-web'
+const App = () => {
+  const [formLogin, setForm] = useState({
+    email: '',
+    password: '',
+  })
 
-import * as React from 'react';
-import { View, Text, Button } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import LoginScreen from './LoginScreen'
-
-function HomeScreen({ navigation}) {
+  const onSubmit = () => {
+    if(formLogin.email === 'agung' && formLogin.password === 'agung')
+      alert('Login Berhasil')
+    } else {
+    alert('Login Gagal')
+  }
+  }
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen</Text>
-      <Button title="Ke Halaman Login" onPress={() => navigation.navigate('LoginScreen')} />
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Form Login</Text>
+      <View>
+        <Text>Email</Text>
+        <TextInput
+          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+          onChangeText={(hasil) => setForm({ ...formLogin, email: hasil })}
+          value={formLogin.email}
+        />
+        <Text>Password</Text>
+        <TextInput
+          style={{ height: 40, borderColor: 'gray', borderWidth: 1 }}
+          onChangeText={(text) => setForm({ ...formLogin, password: text })}
+          value={formLogin.password}
+        />
+        <View style={{ marginTop: 10 }}>
+          <Button title="Login" onPress={() => alert('Login')} />
+        </View>
+        <View>
+          <Text>Email: {formLogin.email}</Text>
+          <Text>Password: {formLogin.password}</Text>
+        </View>
+      </View>
     </View>
-  );
+  )
 }
-
-const Stack = createNativeStackNavigator();
-
-function App() {
-  return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-        <Stack.Screen name="LoginScreen" component={LoginScreen} />
-      </Stack.Navigator>
-    </NavigationContainer> 
-  );
-}
-
-export default App;
+export default App
